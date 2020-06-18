1245a469dff58c7ffceb133526740ca54d09d1bd
evan pan <evan12345pan@hotmail.com>
2020-06-18 19:33:33 -0400

    added autoencoding nn

--- models.py
+++ models.py
@@ -50,9 +50,9 @@ def create_LSTM_model(k, input_shape=[]):
     x = Dense(10)(x)
     model = Model(inputs, x, name="max_rnn")
     return model
+
 def create_LSTM_model(k, input_shape=[]):
     inputs = Input(shape=input_shape)
-    print(inputs.shape)
     x = tf.keras.layers.LSTM(30, go_backwards=True)(inputs)
     x = LeakyReLU()(x)
     x = Dense(20)(x)
@@ -60,6 +60,27 @@ def create_LSTM_model(k, input_shape=[]):
     x = Dense(10)(x)
     model = Model(inputs, x, name="max_rnn")
     return model
+
+def create_encoding_model(k, l, input_shape):
+    inputs = Input(shape=input_shape)
+    x_list = tf.split(inputs, num_or_size_splits=k, axis=1)
+    encoding = Encoder_module(l)(x_list[0])
+    for item in x_list[1:]:
+        encoding = tf.concat((encoding, Encoder_module(l)(item)), axis=1)
+    out = perception_model(encoding, k, 5)
+    model = Model(inputs, out, name="auto_encoder_nn")
+    return model
+
+
+
+def Encoder_module(L):
+    def encoder_module(x):
+        x = Dense(20)(x)
+        x = LeakyReLU()(x)
+        x = Dense(L)(x)
+        x = sigmoid(x) + tf.stop_gradient(tf.math.sign(x) - sigmoid(x))
+        return x
+    return encoder_module
 
 
 
@@ -72,3 +93,6 @@ def perception_model(x, output, layer, logit=True):
         return x
     else:
         return Softmax(x)
+
+
+
