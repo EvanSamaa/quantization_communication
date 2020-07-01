e3a6a0c963e67f4b058cb6688f0e85bc560d6e95
evan pan <evan12345pan@hotmail.com>
2020-07-01 15:51:59 -0400

    e

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
                 print("the accuracy improvement in the past 500 epochs is ", improvement)
                 if improvement <= 0.0001:
                     break
-    fname_template = "trained_models/Sept 29/MLP_loss_function_2{}"
+    fname_template = "trained_models/Sept 29/Conv_net_loss_function_2{}"
     # fname_template = "~/quantization_communication/trained_models/Sept 25th/Data_gen_encoder_L10_hard_tanh{}"
     np.save(fname_template.format(".npy"), graphing_data)
     model = unfreeze_all(model)
