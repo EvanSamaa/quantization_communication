f80887d3aff49b4896cc6a7e4371d667f448dcec
evan pan <evan12345pan@hotmail.com>
2020-08-11 15:58:46 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -92,7 +92,7 @@ def random_complex(shape, sigma2):
 if __name__ == "__main__":
     fname_template = "trained_models/Aug8th/Foad_proposal_1_scaling_test{}"
     check = 200
-    SUPERVISE_TIME = 1000
+    SUPERVISE_TIME = 10
     training_mode = 2
     swap_delay = check/2
     # problem Definition
