a14567c360442c7f85de94687a6edf0ff335a855
evan pan <evan12345pan@hotmail.com>
2020-08-11 15:23:06 -0400

    e

--- models.py
+++ models.py
@@ -1226,9 +1226,9 @@ def FDD_k_times_with_sigmoid_and_penalty(M, K, k=3):
     return model
 def dnn_per_link(input_shape):
     inputs = Input(shape=input_shape)
-    x = Dense(64)(inputs)
+    x = Dense(180)(inputs)
     x = LeakyReLU()(x)
-    x = Dense(64)(x)
+    x = Dense(180)(x)
     x = LeakyReLU()(x)
     # x = Dense(128)(x)
     # x = LeakyReLU()(x)
