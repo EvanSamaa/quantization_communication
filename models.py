84ba50d4114d44c824e5f6663cf8d3b74470a748
evan pan <evan12345pan@hotmail.com>
2020-08-13 18:40:24 -0400

    e

--- models.py
+++ models.py
@@ -1292,10 +1292,10 @@ def FDD_k_times_with_sigmoid_and_penalty(M, K, k=3):
     return model
 def dnn_per_link(input_shape, N_rf):
     inputs = Input(shape=input_shape)
-    x = Dense(1024)(inputs)
+    x = Dense(512)(inputs)
     x = sigmoid(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = Dense(1024)(x)
+    x = Dense(512)(x)
     x = sigmoid(x)
     # x = tf.keras.layers.BatchNormalization()(x)
     # x = Dense(512)(x)
