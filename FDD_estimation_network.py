5dec64575a0775a0e7cf2f3eae97c154da4d73e0
evan pan <evan12345pan@hotmail.com>
2020-08-13 16:11:39 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -102,7 +102,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug9th/Wei_cui_like_model_all_outputs_k=6{}"
+    fname_template = "trained_models/Aug9th/Wei_cui_like_model_all_outputs_k=3{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
