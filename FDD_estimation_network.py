b8e6c09fb928e81646f008097fccc189a574bf44
evan pan <evan12345pan@hotmail.com>
2020-08-13 11:58:55 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -93,7 +93,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug9th/Wei_cui_like_model_with_softmax{}"
+    fname_template = "trained_models/Aug9th/Wei_cui_like_model_with_with_thresholding{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -119,8 +119,8 @@ if __name__ == "__main__":
     # model = FDD_distributed_then_general_architecture(M, K, k=3, N_rf=N_rf)
     # model = FDD_per_link_archetecture(M, K, k=3, N_rf=N_rf)
     # model = FDD_Dumb_model(M, K, k=1, N_rf=N_rf)
-    # model = FDD_per_link_archetecture_sigmoid(M, K, k=3, N_rf=N_rf)
-    model = FDD_per_link_archetecture(M, K, k=3, N_rf=N_rf)
+    model = FDD_per_link_archetecture_sigmoid(M, K, k=3, N_rf=N_rf)
+    # model = FDD_per_link_archetecture(M, K, k=3, N_rf=N_rf)
     optimizer = tf.keras.optimizers.Adam(lr=0.0001)
     # for data visualization
     graphing_data = np.zeros((EPOCHS, 4))
