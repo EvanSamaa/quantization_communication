424f04b79406295f49ea739ee63a138bbb2ff2ce
evan pan <evan12345pan@hotmail.com>
2020-08-15 13:10:13 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -49,8 +49,8 @@ def train_step(features, labels, N=None, epoch=0):
         #     loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         print("==============================")
         loss_3 = Binarization_regularization()(predictions)
-        predictions_hard = predictions + tf.stop_gradient(binary_activation(predictions, shift=0.5) - predictions)
-        loss_4 = OutPut_Limit(N_rf)(predictions_hard)
+        # predictions_hard = predictions + tf.stop_gradient(binary_activation(predictions, shift=0.5) - predictions)
+        loss_4 = OutPut_Limit(N_rf)(predictions)
         loss = loss_1 + loss_2 + loss_4
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -67,7 +67,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug_15th/Feedback_model_baseline+hard_N_RF_constraint{}"
+    fname_template = "trained_models/Aug_15th/Feedback_model_baseline+soft_N_RF_constraint{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
