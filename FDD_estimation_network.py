a260a685412806e532c65abb72e1dce6e0dda3a8
evan pan <evan12345pan@hotmail.com>
2020-08-16 16:02:34 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -46,7 +46,7 @@ def train_step(features, labels, N=None, epoch=0):
             # predictions = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
             # ce = matrix_CE(predictions[:, i], features)
             sr = sum_rate(predictions[:, i], features)
-            vs = vertical_sum(predictions[:, i], features)
+            # vs = vertical_sum(predictions[:, i], features)
             # loss_1 = loss_1 + tf.exp(tf.constant(-predictions.shape[1]-1+i, dtype=tf.float32)) * ce
             loss_1 = loss_1 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * sr
             # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
@@ -70,7 +70,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug_15th/Feedback_model_MP{}"
+    fname_template = "trained_models/Aug_15th/Feedback_model_MP+SM{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -92,8 +92,8 @@ if __name__ == "__main__":
     sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
     # loss_object_1 = Sum_rate_utility_RANKING(K, M, sigma2_n, N_rf)
     vertical_sum = Sum_rate_utility_WeiCui_wrong_axis(K, M, sigma2_n)
-    model = FDD_per_link_archetecture_sigmoid(M, K, k=6, N_rf=N_rf, output_all=True)
-    # model = FDD_per_link_archetecture(M, K, k=6, N_rf=N_rf, output_all=False)
+    # model = FDD_per_link_archetecture_sigmoid(M, K, k=6, N_rf=N_rf, output_all=True)
+    model = FDD_per_link_archetecture(M, K, k=6, N_rf=N_rf, output_all=True)
     optimizer = tf.keras.optimizers.Adam(lr=0.0001)
     # for data visualization
     graphing_data = np.zeros((EPOCHS, 4))
