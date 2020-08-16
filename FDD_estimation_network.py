4b809a7f5ba0971306405c7e93713126298f4ca1
evan pan <evan12345pan@hotmail.com>
2020-08-16 00:58:42 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -68,7 +68,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug_15th/Feedback_model_baseline+hard_N_RF_one_side_constraint{}"
+    fname_template = "trained_models/Aug_15th/Feedback_model_baseline+hard_N_RF_constraint{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
