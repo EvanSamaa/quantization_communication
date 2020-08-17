38dde9cbcf3989d9a91b03660c3a9ab497a4ce74
evan pan <evan12345pan@hotmail.com>
2020-08-17 09:26:02 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -53,10 +53,10 @@ def train_step(features, labels, N=None, epoch=0):
         #     # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         print("==============================")
         # predictions_hard = predictions + tf.stop_gradient(binary_activation(predictions, shift=0.5) - predictions)
-        loss_4 = OutPut_Limit(N_rf)(predictions)
-        # loss_4 = tf.keras.losses.CategoricalCrossentropy()(predictions, mask)
+        # loss_4 = OutPut_Limit(N_rf)(predictions)
+        loss_4 = tf.keras.losses.CategoricalCrossentropy()(predictions, mask)
         loss_3 = Binarization_regularization()(predictions)
-        loss = loss_1
+        loss = loss_1 + loss_4
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     # optimizer.apply_gradients(gradients, model.trainable_variables)
@@ -72,7 +72,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug_15th/Per_user_Feedback_model{}"
+    fname_template = "trained_models/Aug_15th/Per_user_Feedback_model+commitment_loss{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
