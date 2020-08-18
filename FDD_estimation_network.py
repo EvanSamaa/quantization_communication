c88bd22a65e3f0a2d18dfcb213437320b337bdfe
evan pan <evan12345pan@hotmail.com>
2020-08-18 13:04:20 -0400

    e

--- FDD_estimation_network.py
+++ FDD_estimation_network.py
@@ -54,7 +54,7 @@ def train_step(features, labels, N=None, epoch=0):
         # loss_4 = OutPut_Limit(N_rf)(predictions_hard)
         # loss_4 = tf.keras.losses.CategoricalCrossentropy()(predictions, mask)
         loss_3 = Binarization_regularization()(predictions)
-        loss = loss_1
+        loss = loss_1 + loss_4
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     # optimizer.apply_gradients(gradients, model.trainable_variables)
@@ -70,7 +70,7 @@ def random_complex(shape, sigma2):
     A_R.imag = np.random.normal(0, sigma2, shape)
     return A_R
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug_15th/LSTM_perlink_128_model{}"
+    fname_template = "trained_models/Aug_15th/LSTM_perlink_128_model_+commitment_loss{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
