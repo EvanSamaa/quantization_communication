8f8b746ed695acd3b8140c1706a86d700e1132bb
evan pan <evan12345pan@hotmail.com>
2020-08-19 16:59:09 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -53,7 +53,7 @@ def train_step(features, labels, N=None, epoch=0):
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     train_loss(sum_rate(scheduled_output, features))
-    train_binarization_loss(loss_4)
+    # train_binarization_loss(loss_4)
     train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output), features))
 
 
