ca38ef65e887c78ac319bb3351bb972f8321f724
evan pan <evan12345pan@hotmail.com>
2020-08-20 00:42:56 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -38,7 +38,7 @@ def train_step(features, labels, N=None, epoch=0):
         # mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output))
         loss_1 = 0
         loss_2 = VAE_loss()(z_qq, z_e)
-        loss_3 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
+        # loss_3 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
         # loss_4 = tf.keras.losses.CategoricalCrossentropy()(scheduled_output, mask)
         for i in range(0, scheduled_output.shape[1]):
             # predictions = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
@@ -49,7 +49,7 @@ def train_step(features, labels, N=None, epoch=0):
             # loss_4 = loss_4 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * ce
             # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         print("==============================")
-        loss = loss_1 + loss_2 + loss_3
+        loss = loss_1 + loss_2
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     train_loss(sum_rate(scheduled_output[:,-1], features))
@@ -63,7 +63,7 @@ def random_complex(shape, sigma2):
 
 
 if __name__ == "__main__":
-    fname_template = "trained_models/aug19th/B=20E=20+corrected_VAE_feedback+2_layer_per_link_DNN_6_times+MP+reconstructed{}"
+    fname_template = "trained_models/aug19th/B=20E=30+corrected_VAE_feedback+2_layer_per_link_DNN_6_times+MP+reconstructed{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     np.random.seed(seed)
     sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
     # model = Feedbakk_FDD_model_scheduler(M, K, B, 15, N_rf, 6, output_all=True)
-    model = Feedbakk_FDD_model_scheduler_morebit(M, K, B, 20, N_rf, 6, more=2, output_all=True)
+    model = Feedbakk_FDD_model_scheduler_morebit(M, K, B, 30, N_rf, 6, more=2, output_all=True)
     optimizer = tf.keras.optimizers.Adam(lr=0.0001)
     # optimizer = tf.keras.optimizers.SGD(lr=0.001)
     # for data visualization
