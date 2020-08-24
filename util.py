865c74adbc90fa2ecd399decfb208e6c97baf035
evan pan <evan12345pan@hotmail.com>
2020-08-24 12:56:14 -0400

    e

--- util.py
+++ util.py
@@ -824,7 +824,8 @@ class VAE_loss_general():
                         self.M[i] = tf.reduce_sum(tf.concat(choices[i], axis=0), axis=0)
                         self.N[i] = tf.Variable(len(choices[i]), dtype=tf.float32)
                     else:
-                        self.M[i] = None # alternatively adjust M to be the mean of all encoding + some noise
+                        # self.M[i] = None # alternatively adjust M to be the mean of all encoding + some noise
+                        self.M[i] = tf.reduce_mean(z_e, axis=0) + tf.random.normal((z_e.shape[1], ), 0, 0.01)
             else:
                 for i in range(0, len(choices)):
                     if len(choices[i]) != 0 and not (self.M[i] is None):
@@ -834,7 +835,8 @@ class VAE_loss_general():
                         self.M[i] = tf.reduce_sum(tf.concat(choices[i], axis=0), axis=0)
                         self.N[i] = tf.Variable(len(choices[i]), dtype=tf.float32)
                     else:
-                        self.M[i] = None
+                        # self.M[i] = None
+                        self.M[i] = tf.reduce_mean(z_e, axis=0) + tf.random.normal((z_e.shape[1],), 0, 0.01)
             for i in range(0, code_book.shape[1]):
                 if not(self.M[i] is None):
                     self.model.get_layer("Closest_embedding_layer_moving_avg").E[:, i].assign(self.M[i]/self.N[i])
