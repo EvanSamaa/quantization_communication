ce9926056cdfe3b7c9d28fe3dbacc85b0fb58978
evan pan <evan12345pan@hotmail.com>
2020-08-25 10:55:56 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -33,7 +33,8 @@ def train_step(features, labels, N=None, epoch=0):
         train_hard_loss(loss_object_1(Harden_scheduling(k=N_rf)(predictions), features))
         return
     with tf.GradientTape() as tape:
-        scheduled_output, z_qq, z_e, reconstructed_input = model(features)
+        # scheduled_output, z_qq, z_e, reconstructed_input = model(features)
+        reconstructed_input, z_qq, z_e= model(features)
         # scheduled_output, z_q_b, z_e_b, z_q_t, z_e_t, reconstructed_input = model(features)
         # reconstructed_input, z_q_b, z_e_b, z_q_t, z_e_t = model(features)
         # predictions_hard = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
