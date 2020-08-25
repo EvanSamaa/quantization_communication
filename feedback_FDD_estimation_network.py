6b7b5fd49f99436b6955dca55a739898798be85c
evan pan <evan12345pan@hotmail.com>
2020-08-25 11:53:33 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -62,7 +62,7 @@ def train_step(features, labels, N=None, epoch=0):
     # train_binarization_loss(loss_4)
     # train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug25th/Deep_encoder+Deep_decoder+fast{}"
+    fname_template = "trained_models/Aug25th/Deep_encoder+Deep_decoder+batchnorm{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     # model = CSI_reconstruction_model(M, K, B, E, N_rf, 6)
     vae_loss = VAE_loss_general(False)
     sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
-    optimizer = tf.keras.optimizers.Adam(lr=0.001)
+    optimizer = tf.keras.optimizers.Adam(lr=0.0001)
     # optimizer = tf.keras.optimizers.SGD(lr=0.001)
     # for data visualization
     graphing_data = np.zeros((EPOCHS, 4))
