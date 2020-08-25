8af18e836cb7a54ae3d5327a6ae97da63095e135
evan pan <evan12345pan@hotmail.com>
2020-08-25 15:55:27 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -40,7 +40,7 @@ def train_step(features, labels, N=None, epoch=0):
         # predictions_hard = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
         # mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output))
         loss_1 = 0
-        # loss_3 = Reconstruction_loss()(reconstructed_input, tf.abs(features))
+        loss_3 = Reconstruction_loss()(reconstructed_input, tf.abs(features))
         loss_2 = vae_loss.call(z_qq, z_e)
         # loss_4 = tf.keras.losses.CategoricalCrossentropy()(scheduled_output, mask)
         for i in range(0, scheduled_output.shape[1]):
@@ -52,7 +52,7 @@ def train_step(features, labels, N=None, epoch=0):
             # loss_4 = loss_4 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * ce
             # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         # print("==============================")
-        loss = loss_1 + loss_2
+        loss = loss_1 + loss_2 + loss_3
 
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -61,7 +61,7 @@ def train_step(features, labels, N=None, epoch=0):
     # train_binarization_loss(loss_4)
     train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug25th/per_user_Scheduler+B10E10_encoder+outerproduct_in+MP{}"
+    fname_template = "trained_models/Aug25th/per_user_Scheduler+B10E10_encoder+outerproduct_in+MP+commitment_loss{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
