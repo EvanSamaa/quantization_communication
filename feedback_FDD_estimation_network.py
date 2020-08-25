0da967b4664c801a1af8ec1d3b7bcdda9a678f34
evan pan <evan12345pan@hotmail.com>
2020-08-25 15:05:12 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -62,7 +62,7 @@ def train_step(features, labels, N=None, epoch=0):
     # train_binarization_loss(loss_4)
     train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output), features))
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug25th/Scheduler+sigmoid_B10E10_encoder+outerproduct_in+reconstruction{}"
+    fname_template = "trained_models/Aug25th/Scheduler+B10E10_encoder+outerproduct_in+reconstruction{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
