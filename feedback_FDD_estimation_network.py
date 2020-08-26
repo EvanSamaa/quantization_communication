3e2b36a58b9043dfe17fee48aed39a6abcd315c0
evan pan <evan12345pan@hotmail.com>
2020-08-26 17:45:34 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -33,14 +33,14 @@ def train_step(features, labels, N=None, epoch=0):
         train_hard_loss(loss_object_1(Harden_scheduling(k=N_rf)(predictions), features))
         return
     with tf.GradientTape() as tape:
-        # scheduled_output, z_qq, z_e, reconstructed_input = model(features)
-        reconstructed_input, z_qq, z_e= model(features)
+        scheduled_output, z_qq, z_e, reconstructed_input = model(features)
+        # reconstructed_input, z_qq, z_e= model(features)
         # scheduled_output, z_q_b, z_e_b, z_q_t, z_e_t, reconstructed_input = model(features)
         # reconstructed_input, z_q_b, z_e_b, z_q_t, z_e_t = model(features)
         # predictions_hard = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
         # mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output))
-        # loss_1 = tf.keras.losses.MeanSquaredError()(reconstructed_input, sparse_matrix_from_full(tf.abs(features), 5))
-        loss_1 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
+        # loss_1 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
+        loss_1 = sum_rate(scheduled_output, features)
         # loss_3 = Reconstruction_loss()(reconstructed_input, tf.abs(features))
         loss_2 = vae_loss.call(z_qq, z_e)
         # loss_4 = tf.keras.losses.CategoricalCrossentropy()(scheduled_output, mask)
@@ -62,7 +62,7 @@ def train_step(features, labels, N=None, epoch=0):
     # train_binarization_loss(loss_4)
     # train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug25th/B4x3E10code_stacking{}"
+    fname_template = "trained_models/Aug25th/Scheduler+B4x3E10code_stacking{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
