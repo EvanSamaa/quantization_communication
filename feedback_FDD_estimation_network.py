4d89e9f2b7748a5de0120d2315072e1df122facd
evan pan <evan12345pan@hotmail.com>
2020-08-27 13:23:47 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -54,8 +54,8 @@ def train_step(features, labels, N=None, epoch=0):
             loss_4 = loss_4 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce
             # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         # # print("==============================")
-        if tf.reduce_mean(loss_2) >= 15:
-            loss = loss_2
+        if tf.reduce_mean(loss_3) >= 15:
+            loss = loss_3
         else:
             loss = loss_1 + loss_2 + loss_3 + loss_4
 
