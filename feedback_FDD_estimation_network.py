73025f1c718fb977cd7ce6b2fb7d48477b0b0241
evan pan <evan12345pan@hotmail.com>
2020-08-26 22:09:53 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -66,7 +66,7 @@ def train_step(features, labels, N=None, epoch=0):
     train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
     del tape
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug25th/Scheduler+B4x8E10code_stacking+MP+reconstruction_loss+commitment_loss{}"
+    fname_template = "trained_models/Aug26th/Scheduler+B4x{}E10code_stacking+MP+reconstruction_loss+seperate_commitment_loss{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
