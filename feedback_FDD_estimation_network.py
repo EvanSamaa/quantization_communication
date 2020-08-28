30eab44b38690b28fbe70f7ac040ceaa695c5680
evan pan <evan12345pan@hotmail.com>
2020-08-28 13:12:36 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -44,18 +44,18 @@ def train_step(features, labels, N=None, epoch=0):
         # predictions_hard = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
         mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output))
         # loss_1 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
-        loss_1 = sum_rate(scheduled_output, features)
+        loss_1 = 0
         # loss_3 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
         # loss_2 = vae_loss.call(z_qq, z_e)
-        loss_4 = tf.keras.losses.CategoricalCrossentropy()(scheduled_output, mask)
-        # for i in range(0, scheduled_output.shape[1]):
-        #     # predictions = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
-        #     sr = sum_rate(scheduled_output[:, i], features)
-        #     mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output[:, i]))
-        #     ce = tf.keras.losses.CategoricalCrossentropy()(scheduled_output[:, i], mask)
-        #     loss_1 = loss_1 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * sr
-        #     loss_4 = loss_4 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce
-        #     # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
+        loss_4 = 0
+        for i in range(0, scheduled_output.shape[1]):
+            # predictions = predictions + tf.stop_gradient(Harden_scheduling(k=N_rf)(predictions) - predictions)
+            sr = sum_rate(scheduled_output[:, i], features)
+            mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output[:, i]))
+            ce = tf.keras.losses.CategoricalCrossentropy()(scheduled_output[:, i], mask)
+            loss_1 = loss_1 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * sr
+            loss_4 = loss_4 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce
+            # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         # # print("==============================")
         loss = loss_1 + 0.1*loss_4
 
@@ -63,13 +63,13 @@ def train_step(features, labels, N=None, epoch=0):
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     # gradients2 = tape.gradient(loss_4, model.get_layer("model_3").trainable_variables)
     # optimizer2.apply_gradients(zip(gradients2, model.get_layer("model_3").trainable_variables))
-    train_loss(sum_rate(scheduled_output, features))
+    train_loss(sum_rate(scheduled_output[:, -1], features))
     # train_loss(loss_3)
     # train_binarization_loss(loss_3)
-    train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output), features))
+    train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
     del tape
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug27th/512x2_Per_User_Schedular+0.1CE_loss{}"
+    fname_template = "trained_models/Aug27th/512x2_Per_User_Schedular+0.1CE_loss+MP{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     # model = CSI_reconstruction_VQVAE2(M, K, B, E, N_rf, 6, B_t=B_t, E_t=E_t, more=1)
     # model = Feedbakk_FDD_model_scheduler_VAE2(M, K, B, E, N_rf, 6, B_t=B_t, E_t=E_t, more=1, output_all=True)
     model = Feedbakk_FDD_model_scheduler(M, K, B, E, N_rf, 4, more=32, qbit=0, output_all=False)
-    model = FDD_per_user_architecture_double_softmax(M, K, 4, 3, False)
+    model = FDD_per_user_architecture_double_softmax(M, K, 6, N_rf, True)
     # model = tf.keras.models.load_model("trained_models/Aug27th/B4x8E10code_stacking+input_mod.h5", custom_objects=custome_obj)
     # model = CSI_reconstruction_model(M, K, B, E, N_rf, 6)
     vae_loss = VAE_loss_general(False)
