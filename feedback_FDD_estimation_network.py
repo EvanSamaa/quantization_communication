239d88a576905216bf413b61a8379ff0f1b5ea3e
evan pan <evan12345pan@hotmail.com>
2020-08-28 16:57:15 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -71,7 +71,7 @@ def train_step(features, labels, N=None, epoch=0):
     train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
     del tape
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug27th/512x1_Per_User_Schedular+B4x8E10+fine_grain_CE+MP+reconstruction+highLR{}"
+    fname_template = "trained_models/Aug27th/512x1_Per_User_Schedular+B4x8E10+fine_grain_CE+MP+reconstruction+Smol_LR{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -80,8 +80,8 @@ if __name__ == "__main__":
     N = 50
     M = 40
     K = 10
-    B = 4
-    E = 10
+    B = 1
+    E = 4
     B_t = 10
     E_t = 30
     seed = 100
@@ -99,12 +99,12 @@ if __name__ == "__main__":
     # model = Feedbakk_FDD_model_scheduler_VAE2(M, K, B, E, N_rf, 6, B_t=B_t, E_t=E_t, more=1, output_all=True)
     # model = Feedbakk_FDD_model_scheduler(M, K, B, E, N_rf, 6, more=32, qbit=0, output_all=False)
     # model = FDD_per_user_architecture_return_all_softmaxes(M, K, 6, N_rf)
-    model = Feedbakk_FDD_model_scheduler_per_user(M, K, B, E, N_rf, 6, more=8, qbit=0, output_all=True)
+    model = Feedbakk_FDD_model_scheduler_per_user(M, K, B, E, N_rf, 6, more=32, qbit=0, output_all=True)
     # model = tf.keras.models.load_model("trained_models/Aug27th/B4x8E10code_stacking+input_mod.h5", custom_objects=custome_obj)
     # model = CSI_reconstruction_model(M, K, B, E, N_rf, 6)
     vae_loss = VAE_loss_general(False)
     sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
-    optimizer = tf.keras.optimizers.Adam(lr=0.001)
+    optimizer = tf.keras.optimizers.Adam(lr=0.00001)
     optimizer2 = tf.keras.optimizers.Adam(lr=0.0001)
     # optimizer = tf.keras.optimizers.SGD(lr=0.001)
     # for data visualization
