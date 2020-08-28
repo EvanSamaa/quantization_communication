4ed211f04887399308c2acfd712dbed1c8cdfa2e
evan pan <evan12345pan@hotmail.com>
2020-08-28 13:39:51 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -57,7 +57,7 @@ def train_step(features, labels, N=None, epoch=0):
             # loss_4 = loss_4 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce
             # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         # # print("==============================")
-        loss = loss_1 + 0.1*loss_4
+        loss = loss_1 + loss_4
 
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -69,7 +69,7 @@ def train_step(features, labels, N=None, epoch=0):
     train_hard_loss(sum_rate(Harden_scheduling(k=N_rf)(scheduled_output[:, -1]), features))
     del tape
 if __name__ == "__main__":
-    fname_template = "trained_models/Aug27th/512x2_Per_User_Schedular+0.1fine_grain_CE+MP{}"
+    fname_template = "trained_models/Aug27th/512x2_Per_User_Schedular+fine_grain_CE+MP{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
