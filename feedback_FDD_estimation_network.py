289d478accacb2942b3e55128d8dab0dd8feec04
evan pan <evan12345pan@hotmail.com>
2020-08-30 21:44:25 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -63,7 +63,7 @@ def train_step(features, labels, N=None, epoch=0):
             # loss_2 = loss_2 + tf.exp(tf.constant(-predictions.shape[1]+1+i, dtype=tf.float32)) * vs
         # # print("==============================")
         loss = loss_2 + loss_3
-        loss_model_2 = loss_1 + loss_4
+        loss_model_2 = loss_1 + 0.5*loss_4
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     gradients2 = tape.gradient(loss_model_2, model.get_layer("model_2").trainable_variables)
