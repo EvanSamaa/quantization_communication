9a5bc0c7d3e5146532e0af034f2a5e510a08b7e2
evan pan <evan12345pan@hotmail.com>
2020-09-01 10:03:26 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -50,6 +50,7 @@ def train_step(features, labels, N=None, epoch=0):
         loss_3 = tf.keras.losses.MeanSquaredError()(reconstructed_input, tf.abs(features))
         loss_2 = vae_loss.call(z_qq, z_e)
         loss_4 = 0
+        print(N_rf)
         for i in range(0, scheduled_output.shape[1]):
             sr = sum_rate(scheduled_output[:, i], features)
             loss_1 = loss_1 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * sr
@@ -76,7 +77,7 @@ if __name__ == "__main__":
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
-    fname_template = "trained_models/Aug31/M=64_K=50/N_rf=3/VAEB=1x128E=4+1x512_per_linkx6_alt+seperate_doubleCE_loss+MP{}"
+    fname_template = "trained_models/Aug31/M=64_K=50/B=32/N_rf={}+VAEB=1x32E=4+1x512_per_linkx6_alt+seperate_doubleCE_loss+MP{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -95,11 +96,12 @@ if __name__ == "__main__":
     sigma2_n = 0.1
     # hyperparameters
     EPOCHS = 100000
-
-    for i in range(0, 1):
+    mores = [1, 2, 3, 4, 5, 6, 7, 8]
+    for i in mores:
         train_VS = tf.keras.metrics.Mean(name='test_loss')
         tf.random.set_seed(seed)
         np.random.seed(seed)
+        N_rf = i
         # model = CSI_reconstruction_model_seperate_decoders(M, K, B, E, N_rf, 6, more=3, qbit=0)
         # model = CSI_reconstruction_VQVAE2(M, K, B, E, N_rf, 6, B_t=B_t, E_t=E_t, more=1)
         # model = Feedbakk_FDD_model_scheduler_VAE2(M, K, B, E, N_rf, 6, B_t=B_t, E_t=E_t, more=1, output_all=True)
@@ -110,8 +112,7 @@ if __name__ == "__main__":
         # model = CSI_reconstruction_model(M, K, B, E, N_rf, 6, more=32)
         # model = Feedbakk_FDD_model_scheduler_per_user(M, K, B, E, N_rf, 6, 32, output_all=True)
         # model = FDD_per_link_archetecture_more_granular(M, K, 6, N_rf, output_all=True)
-        model = Feedbakk_FDD_model_scheduler(M, K, B, E, N_rf, 6, more=128, qbit=0, output_all=True)
-        print(model.summary())
+        model = Feedbakk_FDD_model_scheduler(M, K, B, E, N_rf, 6, more=32, qbit=0, output_all=True)
         vae_loss = VAE_loss_general(False)
         sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
         optimizer = tf.keras.optimizers.Adam(lr=0.0001)
@@ -147,7 +148,7 @@ if __name__ == "__main__":
             graphing_data[epoch, 3] = train_hard_loss.result()
             if train_hard_loss.result() < max_acc:
                 max_acc = train_hard_loss.result()
-                model.save(fname_template.format(".h5"))
+                model.save(fname_template.format(N_rf, ".h5"))
             if epoch % check == 0:
                 if epoch >= (SUPERVISE_TIME) and epoch >= (check * 2):
                     improvement = graphing_data[epoch - (check * 2): epoch - check, 0].mean() - graphing_data[
@@ -156,7 +157,7 @@ if __name__ == "__main__":
                     print("the accuracy improvement in the past 500 epochs is ", improvement)
                     if improvement <= 0.001:
                         break
-        np.save(fname_template.format(".npy"), graphing_data)
+        np.save(fname_template.format(N_rf,".npy"), graphing_data)
         tf.keras.backend.clear_session()
         print("Training end")
 
