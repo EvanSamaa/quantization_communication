5192a2a08b468dbd796b1de6980653cf932b3d17
evan pan <evan12345pan@hotmail.com>
2020-08-31 21:53:52 -0400

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -66,7 +66,7 @@ def plot_data(arr, col):
     plt.title("Reconstruction Loss")
     plt.show()
 if __name__ == "__main__":
-    file = "trained_models/Aug31/M=64_K=50/N_rf=4/VAEB=1x{}E=4+1x512_per_linkx6_alt+seperate_doubleCE_loss+MP"
+    file = "trained_models/Aug31/M=64_K=50/N_rf=3/VAEB=1x{}E=4+1x512_per_linkx6_alt+seperate_doubleCE_loss+MP"
     custome_obj = {'Closest_embedding_layer': Closest_embedding_layer, 'Interference_Input_modification': Interference_Input_modification,
                    'Interference_Input_modification_no_loop': Interference_Input_modification_no_loop,
                    "Interference_Input_modification_per_user":Interference_Input_modification_per_user,
