e2b923bbf49fda94b91555aa3b4b3a62a24139b6
evan pan <evan12345pan@hotmail.com>
2020-09-01 22:05:54 -0400

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -69,7 +69,7 @@ def plot_data(arr, col):
     plt.title("Reconstruction Loss")
     plt.show()
 if __name__ == "__main__":
-    file = "trained_models/Aug31/M=64_K=50/N_rf=3/VAEB=1x32E=4+1x512_per_linkx6_alt+seperate_doubleCE_loss+MP"
+    file = "trained_models/Aug31/M=64_K=50/B=32/N_rf={}+VAEB=1x32E=4+1x512_per_linkx6_alt+seperate_doubleCE_loss+MP"
     custome_obj = {'Closest_embedding_layer': Closest_embedding_layer, 'Interference_Input_modification': Interference_Input_modification,
                    'Interference_Input_modification_no_loop': Interference_Input_modification_no_loop,
                    "Interference_Input_modification_per_user":Interference_Input_modification_per_user,
@@ -93,18 +93,18 @@ if __name__ == "__main__":
     # plot_data(training_data, 0)
     # model = tf.keras.models.load_model(model_path, custom_objects=custome_obj)
     # N_rfs = [2, 3, 4, 5, 6]
-    mores = [60]
+    mores = [1, 2, 3, 4, 5, 6, 7, 8]
     for i in mores:
-        K=i
+        # K=i
         print("========================================== K =", i)
         # model = partial_feedback_top_N_rf_model(N_rf, B, 1, M, K, sigma2_n)
-        # model = tf.keras.models.load_model(model_path, custom_objects=custome_obj)
+        model = tf.keras.models.load_model(model_path.format(i), custom_objects=custome_obj)
     #     print(model.get_layer("model").summary())
     #     print(model.summary())
         # model = NN_Clustering(N_rf, M, reduced_dim=8)
         # model = k_clustering_hieristic(N_rf)
         # model = greedy_hieristic(N_rf, sigma2_n)
         # model = top_N_rf_user_model(M, K, N_rf)
-        model = partial_feedback_semi_exhaustive_model(N_rf, 32, 10, M, K, sigma2_n)
+        # model = partial_feedback_semi_exhaustive_model(N_rf, 32, 10, M, K, sigma2_n)
         # print(model.summary())
         test_performance(model, M=M, K=K, B=B, N_rf=N_rf, sigma2_n=sigma2_n, sigma2_h = sigma2_h)
