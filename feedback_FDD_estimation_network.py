3f2f7db59f20555f8606a0cb4b508cdb2ffd8a55
evan pan <evan12345pan@hotmail.com>
2020-09-06 21:54:58 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -56,8 +56,8 @@ def train_step(features, labels, N=None, epoch=0):
             sr = sum_rate(scheduled_output[:, i], features)
             loss_1 = loss_1 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * sr
 
-            ce = All_softmaxes_CE_general(N_rf, K, M)(raw_output[:, i])
-            loss_4 = loss_4 + factor[N_rf] * tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce
+            # ce = All_softmaxes_CE_general(N_rf, K, M)(raw_output[:, i])
+            # loss_4 = loss_4 + factor[N_rf] * tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce
 
             mask = tf.stop_gradient(Harden_scheduling(k=N_rf)(scheduled_output[:, i]))
             ce = tf.keras.losses.CategoricalCrossentropy()(scheduled_output[:, i], mask)
@@ -78,13 +78,13 @@ if __name__ == "__main__":
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
-    fname_template = "trained_models/Sept 3rd/K=50,M=64/Naive_model_B=64_weight2_for_both_CE_losses/N_rf={}+B64_1x512_per_linkx6_alt+weighted_double_CE_loss+MP+half_reconstruction{}"
+    fname_template = "trained_models/Sept 3rd/K=50,M=64/Naive_model_B=64_CE1_weight1/N_rf={}+B64_1x512_per_linkx6_alt+weighted_double_CE_loss+MP+half_reconstruction{}"
     check = 300
     SUPERVISE_TIME = 0
     training_mode = 2
     swap_delay = check / 2
     # problem Definition
-    N = 6
+    N = 10
     M = 64
     K = 50
     B = 1
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     sigma2_n = 0.1
     # hyperparameters
     EPOCHS = 100000
-    mores = [1,2,3,4,5,6,7,8]
+    mores = [8, 1,2,3,4,5,6,7]
     for i in mores:
         train_VS = tf.keras.metrics.Mean(name='test_loss')
         tf.random.set_seed(seed)
