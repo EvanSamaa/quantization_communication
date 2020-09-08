46a089ad9d967e24101087b60c09827292dd9b7a
evan pan <evan12345pan@hotmail.com>
2020-09-08 09:36:45 -0400

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
         tf.random.set_seed(seed)
         np.random.seed(seed)
         B = i
-        print("========================================== M =", M)
+        print("========================================== B =", i)
         # model = partial_feedback_top_N_rf_model(N_rf, B, 1, M, K, sigma2_n)
         model = tf.keras.models.load_model(model_path.format(i), custom_objects=custome_obj)
         #     print(model.get_layer("model").summary())
