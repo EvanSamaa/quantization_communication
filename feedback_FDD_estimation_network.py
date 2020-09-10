5cea0dfa28ece984cb220ec9a4686020757f3f4a
evan pan <evan12345pan@hotmail.com>
2020-09-10 10:25:40 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
-    fname_template = "trained_models/Sept8th/K=50,M=64/Naive_model_varying_reconstruction_loss/beta_1x512_per_linkx6_alt+weighted_CE_loss{}"
+    fname_template = "trained_models/Sept8th/K=50,M=64/Naive_model_varying_reconstruction_loss/beta{}_1x512_per_linkx6_alt+weighted_CE_loss{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     sigma2_n = 0.1
     # hyperparameters
     EPOCHS = 100000
-    mores = [100, 10, 1, 0.1, 0.01, 0.001]
+    mores = [0.001, 0.01, 0.1, 1, 10, 50]
     for i in mores:
         train_VS = tf.keras.metrics.Mean(name='test_loss')
         tf.random.set_seed(seed)
