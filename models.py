6d73ed65164d54ead10d3ed395c27ba5fb4d090f
evansamaa <evan12345pan@hotmail.com>
2020-09-21 18:24:47 -0400

    e

--- models.py
+++ models.py
@@ -802,9 +802,7 @@ class Per_link_Input_modification_more_G_less_X(tf.keras.layers.Layer):
         G_mean = tf.reduce_mean(tf.keras.layers.Reshape((self.M*self.K, ))(input_mod), axis=1, keepdims=True)
         G_mean = tf.tile(tf.expand_dims(G_mean, axis=1), (1, self.K * self.M, 1))
         G_user_mean = tf.reduce_mean(input_mod, axis=2, keepdims=True)
-        print(G_user_mean.shape)
         G_user_mean = tf.matmul(self.Mk, G_user_mean)
-        print(G_user_mean.shape)
         affected = tf.matmul(self.Mk, input_mod)
         effectedBy = tf.matmul(self.Mm, tf.transpose(input_mod, (0, 2, 1)))
         x = tf.reduce_sum(x, axis=2)
@@ -2020,7 +2018,7 @@ def FDD_per_link_archetecture_more_G(M, K, k=2, N_rf=3, output_all=False):
     input_mod = tf.square(tf.abs(inputs))
     input_mod = tf.keras.layers.BatchNormalization()(input_mod)
     input_modder = Per_link_Input_modification_more_G_less_X(K, M, N_rf, k)
-    dnns = dnn_per_link((M * K, 6 + 64 +150), N_rf)
+    dnns = dnn_per_link((M * K, 6 + 64 +100), N_rf)
     # compute interference from k,i
     output_0 = tf.stop_gradient(tf.multiply(tf.zeros((K, M)), input_mod[:, :, :]) + 1.0 * N_rf / M / K)
     input_i = input_modder(output_0, input_mod, k - 1.0)
