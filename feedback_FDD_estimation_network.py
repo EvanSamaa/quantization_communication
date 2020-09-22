6c20d97a649afadd7563a267b2294ff26979afc4
evansamaa <evan12345pan@hotmail.com>
2020-09-22 10:47:40 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
-    fname_template = "trained_models/Sept14th/perfect_CSI_fixed_concat_X_even_more_G/Perfect_CSI Nrf={}, 2x512_per_linkx6_alt+weighted_CE_loss{}"
+    fname_template = "trained_models/Sept14th/perfect_CSI_fixed_concat_X_compress_XG/Perfect_CSI Nrf={}, 2x512_per_linkx6_alt+weighted_CE_loss{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
