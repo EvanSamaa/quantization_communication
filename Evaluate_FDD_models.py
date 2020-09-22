e46509a508a83befc66cea08e5c6a6b1dfb795ea
evansamaa <evan12345pan@hotmail.com>
2020-09-22 16:47:00 -0400

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -166,7 +166,7 @@ def plot_data(arr, col=[], title="loss"):
     plt.show()
 if __name__ == "__main__":
 
-    file = "trained_models/Sept14th/perfect_CSI_fixed_concat_X_compress_XG_alt/Perfect_CSI Nrf=8, 2x512_per_linkx6_alt+weighted_CE_loss"
+    file = "trained_models/Sept14th/perfect_CSI_fixed_concat_X_most_G/Perfect_CSI Nrf={}, 2x512+weighted_CE_loss"
     custome_obj = {'Closest_embedding_layer': Closest_embedding_layer, 'Interference_Input_modification': Interference_Input_modification,
                    'Interference_Input_modification_no_loop': Interference_Input_modification_no_loop,
                    "Interference_Input_modification_per_user":Interference_Input_modification_per_user,
@@ -197,7 +197,7 @@ if __name__ == "__main__":
     # N_rfs = [2, 3, 4, 5, 6]
     # model = DP_partial_feedback_semi_exhaustive_model(N_rf, 32, 10, M, K, sigma2_n)
     # test_greedy(model, M=M, K=K, B=B, N_rf=N_rf, sigma2_n=sigma2_n, sigma2_h = sigma2_h)
-    mores = [2,3]
+    mores = [8, 7,6,5,4,3,2,1]
     Es = [0]
     # model = DP_partial_feedback_pure_greedy_model(N_rf, B, 10, M, K, sigma2_n, perfect_CSI=True)
     # test_greedy(model, M=M, K=K, B=B, N_rf=N_rf, sigma2_n=sigma2_n, sigma2_h=sigma2_h)
@@ -205,8 +205,9 @@ if __name__ == "__main__":
         for i in mores:
             tf.random.set_seed(seed)
             np.random.seed(seed)
+            N_rf = i
             print("========================================== E =", j, "more = ", i)
-            model = tf.keras.models.load_model(model_path, custom_objects=custome_obj)
+            model = tf.keras.models.load_model(model_path.format(i), custom_objects=custome_obj)
             # model = partial_feedback_top_N_rf_model(N_rf, B, 1, M, K, sigma2_n)
             #     print(model.get_layer("model").summary())
             #     print(model.summary())
