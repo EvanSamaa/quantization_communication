32d31285d0e4e6ddbe355053c931f8d7f6feb719
evansamaa <evan12345pan@hotmail.com>
2020-10-18 14:35:29 -0400

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template = "trained_models/Oct13/Nrf={}neg_mod+relu+trainmore_beforeswap+more_noise{}"
+    fname_template = "trained_models/Oct13/Nrf={}neg_mod+2xrelu+trainmore_beforeswap+more_noise{}"
     check = 500
     SUPERVISE_TIME = 0
     training_mode = 2
@@ -123,9 +123,9 @@ if __name__ == "__main__":
             # model = Feedbakk_FDD_model_scheduler_naive(M, K, B, E, N_rf, 6, more=more, qbit=0, output_all=True)
             vae_loss = VAE_loss_general(False)
             sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
-            sum_rate_train = Sum_rate_utility_WeiCui(K, M, 50)
-            optimizer = tf.keras.optimizers.Adam(lr=0.001)
-            optimizer2 = tf.keras.optimizers.Adam(lr=0.001)
+            sum_rate_train = Sum_rate_utility_WeiCui(K, M, sigma2_n)
+            optimizer = tf.keras.optimizers.Adam(lr=0.0001)
+            optimizer2 = tf.keras.optimizers.Adam(lr=0.0001)
             # optimizer = tf.keras.optimizers.SGD(lr=0.001)
             # for data visualization
             graphing_data = np.zeros((EPOCHS, 4))
