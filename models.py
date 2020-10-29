479305ee73ca80c6ac29bd9844097bba4cc7115d
evansamaa <evan12345pan@hotmail.com>
2020-10-28 22:23:01 -0400

    e

--- models.py
+++ models.py
@@ -1111,8 +1111,9 @@ class Per_link_Input_modification_most_G_col_lessX(tf.keras.layers.Layer):
         x = tf.tile(x_temp, [1,self.K * self.M, 1])
 
         # x = tf.tile(tf.expand_dims(x, axis=1), (1, self.K * self.M, 1))
-        iteration_num = tf.stop_gradient(tf.multiply(tf.constant(0.0), input_reshaper(input_mod)) + tf.constant(step))
-
+        iteration_num = tf.stop_gradient(tf.multiply(tf.constant(0.0), input_reshaper(input_mod)) + 1)[:, ]
+        num = tf.matmul(self.Mk, tf.ones((self.K, self.K), dtype=tf.float32))
+        num = tf.multiply(tf.tile(iteration_num, [1, 1, self.K]), num)
         input_i = input_concatnator(
             [-input_reshaper(input_mod),
              G_mean, G_max, G_min,
@@ -1121,7 +1122,7 @@ class Per_link_Input_modification_most_G_col_lessX(tf.keras.layers.Layer):
              G_col,
              interference_t, interference_f,
              x, x_current_user,
-             iteration_num])
+             num])
         return input_i
 
     def get_config(self):
@@ -3581,7 +3582,7 @@ def FDD_per_link_2Fold(M, K, k=2, N_rf=3, output_all=False):
     sm = Sparsemax(axis=1)
     # input_modder = Per_link_Input_modification_learnable_G(K, M, N_rf, k)
     dnn1 = dnn_per_link((M * K ,9+K), 1, 0)
-    dnn2 = dnn_per_link((M * K ,11+ K + K), N_rf, 1)
+    dnn2 = dnn_per_link((M * K ,10 + K + K + K), N_rf, 1)
     # compute interference from k,i
     output_0 = tf.stop_gradient(tf.multiply(tf.zeros((K, M)), input_mod[:, :, :]) + 1.0 * N_rf / M / K)
     # raw_out_put_0 = tf.stop_gradient(tf.multiply(tf.zeros((K, M)), input_mod[:, :, :]) + 1.0 / M / K)
