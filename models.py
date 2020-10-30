6841a8dfdc3487c670b72f301065f88062cf4c51
evansamaa <evan12345pan@hotmail.com>
2020-10-29 22:35:46 -0400

    e

--- models.py
+++ models.py
@@ -3626,7 +3626,7 @@ def FDD_per_link_2Fold(M, K, k=2, N_rf=3, output_all=False):
     # sm = Argmax_SPIGOT_layer()
     # input_modder = Per_link_Input_modification_learnable_G(K, M, N_rf, k)
     dnn1 = dnn_per_link((M * K ,9+K), 1, 0)
-    dnn2 = dnn_per_link((M * K ,13 + M*K), N_rf, 1)
+    dnn2 = dnn_per_link((M * K ,10+ M*K + K), N_rf, 1)
     # compute interference from k,i
     output_0 = tf.stop_gradient(tf.multiply(tf.zeros((K, M)), input_mod[:, :, :]) + 1.0 * N_rf / M / K)
     # raw_out_put_0 = tf.stop_gradient(tf.multiply(tf.zeros((K, M)), input_mod[:, :, :]) + 1.0 / M / K)
