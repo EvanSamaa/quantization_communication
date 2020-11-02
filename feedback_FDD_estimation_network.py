64b267a29b479fdd1fef84d59134a69cf4ed3050
evansamaa <evan12345pan@hotmail.com>
2020-11-02 16:25:56 -0500

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -128,8 +128,8 @@ if __name__ == "__main__":
             vae_loss = VAE_loss_general(False)
             sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
             sum_rate_train = Sum_rate_utility_WeiCui(K, M, sigma2_n)
-            optimizer = tf.keras.optimizers.Adam(lr=0.001)
-            optimizer2 = tf.keras.optimizers.Adam(lr=0.001)
+            optimizer = tf.keras.optimizers.Adam(lr=0.01)
+            optimizer2 = tf.keras.optimizers.Adam(lr=0.01)
             # optimizer = tf.keras.optimizers.SGD(lr=0.001)
             # for data visualization
             graphing_data = np.zeros((EPOCHS, 4))
