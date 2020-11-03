9ba71f160a7d9c171adf92a1b7f7b1e87f5bc0d6
evansamaa <evan12345pan@hotmail.com>
2020-11-03 12:26:10 -0500

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -58,10 +58,9 @@ def train_step(features, labels, N=None, epoch=0, lr_boost=1.0):
                 # ce = tf.keras.losses.CategoricalCrossentropy()(scheduled_output[:, i]/N_rf, mask/N_rf)
                 # loss_4 = loss_4 + factor[N_rf]*tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce * lr_boost
                 sr = sum_rate_train(scheduled_output[:, i], features)
-                loss_1 = loss_1 + tf.exp(
-                    tf.constant(-scheduled_output.shape[1] + 1 + i, dtype=tf.float32)) * sr * lr_boost
+                loss_1 = sr * lr_boost
                 ce = tf.reduce_mean(tf.square(tf.multiply(scheduled_output[:, i], 1.0-scheduled_output[:, i])), axis=1)
-                loss_4 = loss_4 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * ce * lr_boost
+                loss_4 = 5 * ce * lr_boost
         # # print("==============================")
         # mask = tf.stop_gradient(Harden_scheduling_user_constrained(1, K, M, default_val=0)(scheduled_output))
         # loss_4 += tf.keras.losses.CategoricalCrossentropy()(scheduled_output/N_rf, mask/N_rf)
