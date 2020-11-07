50efb9024249ef6d788796b2c462344520f06d86
evansamaa <evan12345pan@hotmail.com>
2020-11-07 18:56:24 -0500

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -47,11 +47,10 @@ def train_step(features, labels, N=None, epoch=0, lr_boost=1.0):
         # loss_4 = tf.reduce_mean(tf.square(tf.multiply(scheduled_output, 1.0-scheduled_output)), axis=1)
         # loss_4 = tf.reduce_mean(tf.square(tf.multiply(scheduled_output[:, -1], 1.0-scheduled_output[:, -1])), axis=1)
         for i in range(0, scheduled_output.shape[1]-1):
-            mask = tf.stop_gradient(Harden_scheduling_user_constrained(1, K, M, default_val=0)(scheduled_output[:, i]))
+            mask = tf.stop_gradient(Harden_scheduling_user_constrained(i+1, K, M, default_val=0)(scheduled_output[:, i]))
             # sr = sum_rate_hard(scheduled_output[:, i], mask, features)
             sr = sum_rate_train(scheduled_output[:, i], features)
-            # ce = tf.keras.losses.CategoricalCrossentropy()(scheduled_output[:, i] / N_rf, mask / N_rf)
-            ce = All_softmaxes_CE_general(N_rf, K, M)(raw_output[:,i])
+            ce = tf.keras.losses.CategoricalCrossentropy()(scheduled_output[:, i] / N_rf, mask / N_rf)
             # ce = tf.reduce_mean(tf.square(tf.multiply(scheduled_output[:, i], 1.0-scheduled_output[:, i])), axis=1)
             # loss_1 = loss_1 + tf.exp(tf.constant(-scheduled_output.shape[1]+1+i, dtype=tf.float32)) * sr
             loss_1 = loss_1 + sr
