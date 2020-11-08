187dc56dbbfc27c36059266a2e8705c51a9c85d6
evansamaa <evan12345pan@hotmail.com>
2020-11-07 19:59:44 -0500

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template = "trained_models/OCT30/Nrf=4/seeding={}one_hot_with_new_input_mod{}"
+    fname_template = "trained_models/OCT30/Nrf=4/seeding={}one_hot_with_new_input_mod+2_times{}"
     check = 250
     SUPERVISE_TIME = 0
     training_mode = 2
