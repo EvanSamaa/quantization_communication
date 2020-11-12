4c20bc7dcf81d14a11338cd54013c7270f9d23a8
evansamaa <evan12345pan@hotmail.com>
2020-11-12 02:50:24 -0500

    e

--- models.py
+++ models.py
@@ -3977,7 +3977,7 @@ def FDD_one_at_a_time_iterable(M, K, k=2, N_rf=3, output_all=False):
     # norm = tf.reduce_max(tf.keras.layers.Reshape((K * M,))(input_mod), axis=1, keepdims=True)
     mean = tf.reduce_mean(tf.keras.layers.Reshape((K * M,))(input_mod), axis=1, keepdims=True)
     std = tf.math.reduce_std(tf.keras.layers.Reshape((K * M,))(input_mod), axis=1, keepdims=True)
-    input_mod = tf.divide(input_mod - mean, std)
+    input_mod = tf.divide(input_mod - tf.expand_dims(mean, axis=1), tf.expand_dims(std, axis=1))
     input_modder = Sequential_Per_link_Input_modification_most_G_raw_self(K, M, N_rf, 2*N_rf)
     dnn_model = dnn_sequential((K*M, 17 + 2*N_rf))
     output_final = tf.stop_gradient(tf.multiply(tf.zeros((K, M)), input_mod[:, :, :]) + 1.0) # inital output/planning
