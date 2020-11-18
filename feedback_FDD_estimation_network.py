541ee02abb7ca5e09c4b3b542f10e1780e82186a
evansamaa <evan12345pan@hotmail.com>
2020-11-17 22:50:56 -0500

    e

--- feedback_FDD_estimation_network.py
+++ feedback_FDD_estimation_network.py
@@ -74,7 +74,7 @@ def train_step(features, labels, N=None, epoch=0, lr_boost=1.0, reg_strength = 1
         # ================================= middle iterations =================================
 
         # loss = loss_2 + loss_3
-        loss = loss_1 + loss_4
+        loss = loss_1 + 0.01 * loss_4
         # loss_4 = factor[N_rf] * loss_4 + loss_1
     # gradients = tape.gradient(loss, model.trainable_variables)
     # optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template = "trained_models/Nov_15/sigmoid+user_reg+Nrf_reg{}"
+    fname_template = "trained_models/Nov_15/sigmoid+smol_user_reg+Nrf_reg{}"
     check = 250
     SUPERVISE_TIME = 0
     training_mode = 2
