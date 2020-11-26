3986b855054f60bb44aafb029287a75cc2a47ae8
evansamaa <evan12345pan@hotmail.com>
2020-11-26 15:51:42 -0500

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     #     training = training[:i, :]
     #     print("dim = {} gives MSE of {} ".format(i, training[-1, 1]))
     # A[2]
-    file = "trained_models/Nov_23/SNR=2_Nrf={}more={}naive_64x2_withgradient_flowall"
+    file = "trained_models/Nov_23/SNR=2_Nrf={}more={}naive_64x2"
 
     # file = "trained_models/Nov_23/B=32_one_CE_loss/N_rf=1+VAEB=1x32E=4+1x512_per_linkx6_alt+CE_loss+MP"
     # for item in [0.01, 0.1, 1, 5, 10]:
