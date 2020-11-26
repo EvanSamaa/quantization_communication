95c9341836ad78e2a42669a367ad5e3d195bc251
evansamaa <evan12345pan@hotmail.com>
2020-11-26 00:47:43 -0500

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -118,7 +118,7 @@ def test_performance(model, M = 20, K = 5, B = 10, N_rf = 5, sigma2_h = 6.3, sig
         # prediction = model(ds_load)
         # compressed_G, position_matrix = G_compress(ds_load, 2)
         # scheduled_output, raw_output = model.predict_on_batch([ds_load, compressed_G, position_matrix])
-        scheduled_output, raw_output, reconstructed_input = model.predict_on_batch(ds_load, batch_size=5)
+        scheduled_output, raw_output, reconstructed_input = model.predict(ds_load, batch_size=5)
         # scheduled_output, raw_output, input_mod, input_reconstructed_mod, reconstructed_input = model.predict_on_batch(ds_load)
 
         # scheduled_output, raw_output, recon = model(ds_load)
