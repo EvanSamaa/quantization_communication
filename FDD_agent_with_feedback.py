5068f4d9e8f880caaf96d989620a6c5266a407c7
evansamaa <evan12345pan@hotmail.com>
2020-12-14 19:55:17 -0500

    e

--- FDD_agent_with_feedback.py
+++ FDD_agent_with_feedback.py
@@ -34,11 +34,11 @@ def grid_search(N_rf = 8):
     ################################ hyperparameters ###############################
     EPOCHS = 100000
     lr = 0.001
-    N = 50 # number of
+    N = 30 # number of
     rounds = 5
     sample_size = 100
     temp = 0.1
-    check = 50
+    check = 200
     # model = FDD_per_link_archetecture_more_G(M, K, 7, N_rf, True, max_val)
     model = Feedbakk_FDD_model_scheduler_naive(M, K, B, E, N_rf, 6, more=more, avg_max=max_val)
     optimizer = tf.keras.optimizers.Adam(lr=lr)
