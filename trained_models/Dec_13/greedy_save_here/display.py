97bbcefb036db3cdbc8a4dc60ada21eeee3216a7
evansamaa <evan12345pan@hotmail.com>
2020-12-15 17:15:55 -0500

    e

--- trained_models/Dec_13/greedy_save_here/display.py
+++ trained_models/Dec_13/greedy_save_here/display.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-if __name__ == "__main__":
-
+def display2d():
     grid = np.load("grid_search.npy")
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -9,10 +8,11 @@ if __name__ == "__main__":
     x = np.array([1, 2, 5, 10, 64]) # links
     y = np.array([1, 2, 4, 8, 16, 32]) # bits
     X, Y = np.meshgrid(x, y)
+    print(Y)
     zs = np.zeros((len(x), len(y)), dtype=np.float32)
     # delete the for looop to display only one Nrf
     #
-    for N in range(8):
+    for N in range(7,8):
         for i in range(len(x)):
             for j in range(len(y)):
                 zs[i, j] = -grid[x[i] - 1, y[j] - 1, N]
@@ -22,4 +22,24 @@ if __name__ == "__main__":
     ax.set_ylabel('bits')
     ax.set_zlabel('SR')
 
+    plt.show()
+if __name__ == "__main__":
+    grid = np.load("grid_search.npy")
+    fig = plt.figure()
+    # add or remove points using x and y
+    x = np.array([1, 2, 5, 10])  # links
+    y = np.array([1, 2, 4, 8, 16, 32])  # bits
+    Nrf = 5
+    curves = []
+    vals = []
+    for i in x:
+        new_curve = []
+        new_val = []
+        for j in y:
+            new_curve.append(i*(6+j))
+            new_val.append(-grid[i-1,j-1,Nrf-1])
+        curves.append(np.array(new_curve))
+        vals.append(np.array(new_val))
+        plt.plot(np.array(new_curve), np.array(new_val), label="{} links".format(i))
+    plt.legend()
     plt.show()
