0c7a7ee39b775dbe915b457d7b145ef4d4cd0aa0
evansamaa <evan12345pan@hotmail.com>
2020-12-16 01:54:06 -0500

    e

--- FDD_agent_with_feedback.py
+++ FDD_agent_with_feedback.py
@@ -10,7 +10,7 @@ def grid_search(N_rf = 8):
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template_template = "trained_models/Dec_13/with_feedback/it64/GNN_annealing_temp_Nrf={}+limit_res=2".format(N_rf)
+    fname_template_template = "trained_models/Dec_13/with_feedback/it64/GNN_annealing_temp_Nrf={}+limit_res=6".format(N_rf)
     fname_template = fname_template_template + "{}"
     check = 250
     SUPERVISE_TIME = 0
@@ -27,7 +27,7 @@ def grid_search(N_rf = 8):
     # N_rf = 8
     sigma2_h = 6.3
     sigma2_n = 1.0
-    res = 2
+    res = 6
     ############################### generate data ###############################
     valid_data = generate_link_channel_data(1000, K, M, Nrf=N_rf)
     garbage, max_val = Input_normalization_per_user(tf.abs(valid_data))
