fa5820a71846bcbfec96c95e3d76b614d05e8e87
evansamaa <evan12345pan@hotmail.com>
2021-01-05 22:37:53 -0500

    e

--- FDD_agent_with_feedback.py
+++ FDD_agent_with_feedback.py
@@ -168,7 +168,7 @@ def grid_search_STD(more = 8):
     temp = 0.1
     check = 200
     # model = FDD_per_link_archetecture_more_G(M, K, 7, N_rf, True, max_val)
-    model = FDD_per_link_archetecture_more_G(M, K, B, E, N_rf, 4, more=more, avg_max=max_val)
+    model = Feedbakk_FDD_model_scheduler_naive(M, K, B, E, N_rf, 4, more=more, avg_max=max_val)
     optimizer = tf.keras.optimizers.Adam(lr=lr)
     ################################ Metrics  ###############################
     sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
