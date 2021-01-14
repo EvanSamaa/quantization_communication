a2462e139cb4d2e63a4b63d1a0b4777d6e67eb62
evansamaa <evan12345pan@hotmail.com>
2021-01-14 10:01:22 -0500

    e

--- Evaluate_FDD_models.py
+++ Evaluate_FDD_models.py
@@ -527,7 +527,7 @@ if __name__ == "__main__":
                    "Sequential_Per_link_Input_modification_most_G_raw_self":Sequential_Per_link_Input_modification_most_G_raw_self,
                    "Per_link_Input_modification_most_G_raw_self_sigmoid":Per_link_Input_modification_most_G_raw_self_sigmoid}
 
-    partial_feedback_and_DNN_grid_search()
+    greedy_grid_search()
     A[2]
     # training_data = np.load("trained_models\Dec_13\GNN_grid_search_temp=0.1.npy")
     # plot_data
