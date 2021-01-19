1456f702aac1c7015c822ff60a1e9dafbf05b28d
evansamaa <evan12345pan@hotmail.com>
2021-01-18 22:56:32 -0500

    e

--- models.py
+++ models.py
@@ -4191,9 +4191,9 @@ def FDD_agent_more_G(M, K, k=2, N_rf=3, normalization=True, avg_max=None):
         x = Dense(128, name="Dense1_inside_DNN{}".format(i))(inputs)
         x = tf.keras.layers.BatchNormalization(name="batchnorm_inside_DNN{}".format(i))(x)
         x = sigmoid(x)
-        x = Dense(128, name="Dense2_inside_DNN{}".format(i))(x)
-        x = tf.keras.layers.BatchNormalization(name="batchnorm_inside_DNN_2{}".format(i))(x)
-        x = sigmoid(x)
+        # x = Dense(128, name="Dense2_inside_DNN{}".format(i))(x)
+        # x = tf.keras.layers.BatchNormalization(name="batchnorm_inside_DNN_2{}".format(i))(x)
+        # x = sigmoid(x)
         x = Dense(N_rf, name="Dense4_inside_DNN{}".format(i))(x)
         model = Model(inputs, x, name="DNN_within_model{}".format(i))
         return model
