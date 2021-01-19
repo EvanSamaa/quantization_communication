b595a8ba2ff94fa7b4e139c9f8532fcce177dbdf
evansamaa <evan12345pan@hotmail.com>
2021-01-18 23:43:26 -0500

    e

--- FDD_agent.py
+++ FDD_agent.py
@@ -156,7 +156,7 @@ def grid_search_relax(N_rf = 8):
         train_data = generate_link_channel_data_fullAOE(N, K, M, Nrf=N_rf)
         ###################### training happens here ######################
         for e in range(0, rounds):
-            temp = 0.5 * np.exp(-4.5 / rounds * e) * tf.maximum(0.0, ((400.0-i)/400.0)) + 0.05
+            temp = 0.5 * np.exp(-4.5 / rounds * e) * tf.maximum(0.0, ((200.0-i)/200.0)) + 0.05
             temp = np.float32(temp)
             train_hard_loss.reset_states()
             train_loss.reset_states()
