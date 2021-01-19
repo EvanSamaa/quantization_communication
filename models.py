7d94d169212771bae641c415300eddd1ad9f2d20
evansamaa <evan12345pan@hotmail.com>
2021-01-18 22:53:03 -0500

    e

--- models.py
+++ models.py
@@ -4186,7 +4186,7 @@ def FDD_per_link_archetecture_more_G(M, K, k=2, N_rf=3, normalization=True, avg_
     model = Model(inputs, output, name="scheduler")
     return model
 def FDD_agent_more_G(M, K, k=2, N_rf=3, normalization=True, avg_max=None):
-    def self_agent_dnn(input_shape):
+    def self_agent_dnn(input_shape, i=0):
         inputs = Input(shape=input_shape, name="DNN_input_insideDNN{}".format(i))
         x = Dense(128, name="Dense1_inside_DNN{}".format(i))(inputs)
         x = tf.keras.layers.BatchNormalization(name="batchnorm_inside_DNN{}".format(i))(x)
