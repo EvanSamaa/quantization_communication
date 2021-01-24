f80dd7b60d3960ecafff0250c8b998ed4c7aabc9
evansamaa <evan12345pan@hotmail.com>
2021-01-24 03:55:24 -0500

    e

--- FDD_agent.py
+++ FDD_agent.py
@@ -276,7 +276,8 @@ def grid_search_with_mutex_loss(N_rf = 8):
                 out = tf.reshape(out, [sample_size*N, K*M])
                 train_label = tf.reshape(tf.tile(tf.expand_dims(train_data, axis=0), [sample_size,1, 1, 1]), [sample_size*N, K, M])
                 ###################### model post-processing ######################
-                loss = train_sum_rate(out, train_label) + 0.01*mutex_loss_fn(out)
+                loss = train_sum_rate(out, train_label) + 0.01*mutex_loss_fn(ans[:, -1])
+                # loss = train_sum_rate(out, train_label) + 0.01 *mutex_loss_fn(out)
             gradients = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients,model.trainable_variables))
             # optimizer.minimize(loss, ans)
