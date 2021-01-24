893155701b5ed60f421613cdea797f5f16255b74
evansamaa <evan12345pan@hotmail.com>
2021-01-24 03:49:09 -0500

    e

--- FDD_agent.py
+++ FDD_agent.py
@@ -214,7 +214,7 @@ def grid_search_with_mutex_loss(N_rf = 8):
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template_template = "trained_models/Jan_18/test_DNN_empty_choice".format(N_rf)
+    fname_template_template = "trained_models/Jan_18/test_DNN_tiny".format(N_rf)
     fname_template = fname_template_template + "{}"
     check = 250
     SUPERVISE_TIME = 0
