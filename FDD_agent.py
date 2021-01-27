701a2e3f26015027a3fbc900f0e1b56036525305
evansamaa <evan12345pan@hotmail.com>
2021-01-26 23:31:26 -0500

    e

--- FDD_agent.py
+++ FDD_agent.py
@@ -216,7 +216,7 @@ def grid_search_with_mutex_loss(N_rf = 8):
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template_template = "trained_models/Jan_18/test_dnn_Nrf={}+annealing_LR".format(N_rf)
+    fname_template_template = "trained_models/Jan_18/test_dnn_Nrf={}+annealing_LR_Nadam".format(N_rf)
     fname_template = fname_template_template + "{}"
     check = 250
     SUPERVISE_TIME = 0
@@ -244,7 +244,7 @@ def grid_search_with_mutex_loss(N_rf = 8):
     temp = 0.1
     check = 100
     model = FDD_agent_more_G(M, K, 5, N_rf, True, max_val)
-    optimizer = tf.keras.optimizers.Adam(lr=lr)
+    optimizer = tf.keras.optimizers.Nadam(lr=lr)
     ################################ Metrics  ###############################
     sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
     train_sum_rate = Sum_rate_utility_WeiCui(K, M, sigma2_n)
@@ -310,7 +310,7 @@ def grid_search_with_mutex_loss(N_rf = 8):
                 print("the improvement in the past 500 epochs is: ", improvement)
                 print("the validation SR is: ", valid_loss)
                 if improvement <= 0.0001 and lr == 0.001:
-                    optimizer = tf.keras.optimizers.Adam(lr=0.0001)
+                    optimizer = tf.keras.optimizers.Nadam(lr=0.0001)
                 elif improvement <= 0.0001 and lr < 0.001:
                     break
         else:
