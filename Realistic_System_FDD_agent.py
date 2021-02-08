30ffe52c5293a192e6973f63e4081063898b804e
evansamaa <evan12345pan@hotmail.com>
2021-02-08 10:03:40 -0500

    e

--- Realistic_System_FDD_agent.py
+++ Realistic_System_FDD_agent.py
@@ -12,7 +12,7 @@ def grid_search_with_mutex_loss(N_rf = 8):
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
     # fname_template = "trained_models/Sept23rd/Nrf=4/Nrf={}normaliza_input_0p25CE+residual_more_G{}"
-    fname_template_template = "trained_models/Feb8th/random_user_loc/on_user_loc_0_Nrf=".format(N_rf)
+    fname_template_template = "trained_models/Feb8th/random_user_loc/on_user_loc_0_Nrf={}".format(N_rf)
     fname_template = fname_template_template + "{}"
     check = 250
     SUPERVISE_TIME = 0
