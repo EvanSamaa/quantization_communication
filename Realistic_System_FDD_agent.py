fb0758e6ebe1c7874f825d507d9886af97979db0
evansamaa <evan12345pan@hotmail.com>
2021-02-15 15:31:19 -0500

    e

--- Realistic_System_FDD_agent.py
+++ Realistic_System_FDD_agent.py
@@ -208,7 +208,7 @@ def grid_search_with_mutex_loss_episodic(N_rf = 8):
                     train_label = tf.reshape(tf.tile(tf.expand_dims(train_data, axis=0), [sample_size,1, 1, 1]), [sample_size*N, K, M])
                     weight = tf.reshape(tf.tile(tf.expand_dims(env.get_weight(), axis=0), [sample_size,1, 1, 1]), [sample_size*N, K, M])
                     ###################### model post-processing ######################
-                    loss = env.compute_weighted_loss(out, train_label, weight=weight update=False) + mutex_loss_fn(raw_ans[:, -1])
+                    loss = env.compute_weighted_loss(out, train_label, weight=weight, update=False) + mutex_loss_fn(raw_ans[:, -1])
                     env.compute_weighted_loss(ans[:, -1], train_label, update=True)
                     env.increment()
                     print(tf.reduce_mean(tf.reduce_sum(env.rates, axis=2)))
