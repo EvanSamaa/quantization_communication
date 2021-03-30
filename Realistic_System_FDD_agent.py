c6d728d5213828258e1464fa64d4c5dfd54db51d
evansamaa <evan12345pan@hotmail.com>
2021-03-30 00:54:11 -0400

    e

--- Realistic_System_FDD_agent.py
+++ Realistic_System_FDD_agent.py
@@ -489,7 +489,7 @@ def grid_search_with_mutex_loss_weighted_sumrate_train_as_if_non_episodic(N_rf =
     ################################ hyperparameters ###############################
     EPOCHS = 100000
     lr = 0.001
-    N = 24 # number of
+    N = 15 # number of
     rounds = 8
     sample_size = 15
     temp = 0.1
@@ -559,8 +559,8 @@ def grid_search_with_mutex_loss_weighted_sumrate_train_as_if_non_episodic(N_rf =
                                 axis=2)
             scheduled_output = []
             raw_output = []
-            for batches in range(0, math.floor(valid_data.shape[0]/20)):
-                scheduled_output_temp, raw_output_temp = model(input_mod[batches * 20:(batches + 1) * 20, :, :])
+            for batches in range(0, math.floor(valid_data.shape[0]/10)):
+                scheduled_output_temp, raw_output_temp = model(input_mod[batches * 10:(batches + 1) * 10, :, :])
                 scheduled_output.append(scheduled_output_temp)
                 raw_output.append(raw_output_temp)
             scheduled_output = tf.concat(scheduled_output, axis = 0)
